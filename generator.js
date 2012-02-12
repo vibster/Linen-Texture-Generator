af21941215f12772e2237f2e75f0cbecad66f1a0
Alex Usbergo <alexakadrone@gmail.com>
2012-02-12 16:46:14 +0100

    first

---
+++ generator.js
@@ -0,0 +1,11 @@
+$(function(){
+
+	//canvas
+	var canvas = document.getElementById("canvas");
+ 	var context = canvas.getContext("2d");	
+
+	context.lineWidth = 30;
+	context.strokeStyle = “#78c2c5″;
+	context.stroke();
+
+});

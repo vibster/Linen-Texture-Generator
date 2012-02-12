f6cd89aca269f46fb82f4beab43e620b5d2739a4
Valerio Di Donato <valeriodidonato@gmail.com>
2012-02-12 23:42:50 +0100

    added some tests for the motion blur library

--- generator.js
+++ generator.js
@@ -5,88 +5,91 @@ $(function(){
 	generateNoise("canvas-b");
 	generateEmpty("canvas-c");
 
-	blendTest();
-
-	function generateNoise(canvasId) {
-		
-		//canvas
-		var canvas = document.getElementById(canvasId);
-	 	var ctx = canvas.getContext('2d');
-		var x, y, r, g, b, opacity = opacity || 1.0;
-
-		canvas.width = 500;
-		canvas.height = 500;
-		
-		ctx.fillStyle = '#cacaca';
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
-	         
-	 	for (x = 0; x < canvas.width; x++) {
-			for (y = 0; y < canvas.height; y++) {
-				number = Math.floor( Math.random() * 256 );
-				ctx.fillStyle = 'rgba(' + number + ',' + number + ',' + number + ',' + opacity + ')';
-				ctx.fillRect(x, y, 1, 1);
-	 		}
-	 	}
-
-	}
-
-	function generateEmpty(canvasId) {
-		//canvas
-		var canvas = document.getElementById(canvasId);
-	 	var ctx = canvas.getContext('2d');
-		var x, y, r, g, b, opacity = opacity || 1.0;
-
-		canvas.width = 500;
-		canvas.height = 500;
-		
-		ctx.fillStyle = '#cacaca';
-		ctx.fillRect(0, 0, canvas.width, canvas.height);
-	         
-	 	for (x = 0; x < canvas.width; x++) {
-			for (y = 0; y < canvas.height; y++) {
-				number = Math.floor( Math.random() * 256 );
-				ctx.fillStyle = 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + opacity + ')';
-				ctx.fillRect(x, y, 1, 1);
-	 		}
-	 	}
-	 }
-
-	function blendTest() {
-
-		var imageBottom = document.getElementById('canvas');
-		var imageTop = document.getElementById('canvas-b');
-		
-		var imageFinal = document.getElementById("canvas-c");
-
-		ctxBottom = imageBottom.getContext('2d');
-		ctxTop = imageTop.getContext('2d');
-		ctxFinal = imageFinal.getContext('2d');
-		
-		// Get the CanvasPixelArray from the given coordinates.
-	    imgBottomData = ctxBottom.getImageData(0, 0, imageBottom.width, imageBottom.height);
-		imgTopData 	  = ctxTop.getImageData(0, 0, imageTop.width, imageTop.height);
-		imgFinalData  = ctxFinal.getImageData(0, 0, imageFinal.width, imageFinal.height);
-		
-		
-	    for (var i = 0, n = imgBottomData.data.length; i < n; i += 4) {
-	        imgFinalData.data[i  ] = imgBottomData.data[i  ] * imgTopData.data[i  ] / 255.0; //red
-	        imgFinalData.data[i+1] = imgBottomData.data[i+1] * imgTopData.data[i+1] / 255.0; //green
-	        imgFinalData.data[i+2] = imgBottomData.data[i+2] * imgTopData.data[i+2] / 255.0; //blue
-	        // pix[i+3] is alpha channel (ignored)
-	    }
-
-
-	    /*
-	    for (var i = 0, n = imgBottomData.data.length; i < n; i += 4) {
-	        imgFinalData.data[i  ] = 255 - imgBottomData.data[i  ]; //red
-	        imgFinalData.data[i+1] = 255 - imgBottomData.data[i+1]; //green
-	        imgFinalData.data[i+2] = 255 - imgBottomData.data[i+2]; //blue
-	        // pix[i+3] is alpha channel (ignored)
-	    }
-	   */
-
-	    // Draw the result on the canvas
-	    ctxFinal.putImageData(imgFinalData, 0, 0);	
-	};
+    blendTest();
 
 });
+
+function generateNoise(canvasId) {
+	
+	//canvas
+	var canvas = document.getElementById(canvasId);
+ 	var ctx = canvas.getContext('2d');
+	var x, y, r, g, b, opacity = opacity || 1.0;
+
+	canvas.width = 500;
+	canvas.height = 500;
+	
+	ctx.fillStyle = '#cacaca';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+         
+ 	for (x = 0; x < canvas.width; x++) {
+		for (y = 0; y < canvas.height; y++) {
+			number = Math.floor( Math.random() * 256 );
+			ctx.fillStyle = 'rgba(' + number + ',' + number + ',' + number + ',' + opacity + ')';
+			ctx.fillRect(x, y, 1, 1);
+ 		}
+ 	}
+
+ 	var test = new motionBlur();
+ 	test.filter(ctx.getImageData(0, 0, canvas.width, canvas.height));
+
+}
+
+function generateEmpty(canvasId) {
+	//canvas
+	var canvas = document.getElementById(canvasId);
+ 	var ctx = canvas.getContext('2d');
+	var x, y, r, g, b, opacity = opacity || 1.0;
+
+	canvas.width = 500;
+	canvas.height = 500;
+	
+	ctx.fillStyle = '#cacaca';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+         
+ 	for (x = 0; x < canvas.width; x++) {
+		for (y = 0; y < canvas.height; y++) {
+			number = Math.floor( Math.random() * 256 );
+			ctx.fillStyle = 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + opacity + ')';
+			ctx.fillRect(x, y, 1, 1);
+ 		}
+ 	}
+ }
+
+function blendTest() {
+
+	var imageBottom = document.getElementById('canvas');
+	var imageTop = document.getElementById('canvas-b');
+	
+	var imageFinal = document.getElementById("canvas-c");
+
+	ctxBottom = imageBottom.getContext('2d');
+	ctxTop = imageTop.getContext('2d');
+	ctxFinal = imageFinal.getContext('2d');
+	
+	// Get the CanvasPixelArray from the given coordinates.
+    imgBottomData = ctxBottom.getImageData(0, 0, imageBottom.width, imageBottom.height);
+	imgTopData 	  = ctxTop.getImageData(0, 0, imageTop.width, imageTop.height);
+	imgFinalData  = ctxFinal.getImageData(0, 0, imageFinal.width, imageFinal.height);
+	
+	
+    for (var i = 0, n = imgBottomData.data.length; i < n; i += 4) {
+        imgFinalData.data[i  ] = imgBottomData.data[i  ] * imgTopData.data[i  ] / 255.0; //red
+        imgFinalData.data[i+1] = imgBottomData.data[i+1] * imgTopData.data[i+1] / 255.0; //green
+        imgFinalData.data[i+2] = imgBottomData.data[i+2] * imgTopData.data[i+2] / 255.0; //blue
+        // pix[i+3] is alpha channel (ignored)
+    }
+
+
+    /*
+    for (var i = 0, n = imgBottomData.data.length; i < n; i += 4) {
+        imgFinalData.data[i  ] = 255 - imgBottomData.data[i  ]; //red
+        imgFinalData.data[i+1] = 255 - imgBottomData.data[i+1]; //green
+        imgFinalData.data[i+2] = 255 - imgBottomData.data[i+2]; //blue
+        // pix[i+3] is alpha channel (ignored)
+    }
+   */
+
+    // Draw the result on the canvas
+    ctxFinal.putImageData(imgFinalData, 0, 0);	
+};
--- motionblur.js
+++ motionblur.js
@@ -1,64 +1,60 @@
-$(function(){
+// constructor
+function motionBlur(angle, falloff, distance, wrapEdges, rotation) {
 
-	// constructor
-	function motionBlur(angle, falloff, distance, wrapEdges, rotation) {
+	this.angle     = angle     || 0;
+	this.falloff   = falloff   || 1;
+	this.distance  = distance  || 1;
+	this.wrapEdges = wrapEdges || false;
+	this.rotation  = rotation  || 0;
+	
+};
 
-		this.angle     = angle     || 0;
-		this.falloff   = falloff   || 1;
-		this.distance  = distance  || 1;
-		this.wrapEdges = wrapEdges || false;
-		this.rotation  = rotation  || 0;
-		
-	};
+motionBlur.prototype.filter = function(src, dst) {
+	var width, height, inPixels, outPixels, 
+		sinAngle, cosAngle, total, cx, cy, 
+		index, imageRadius, translateX, 
+		translateY, maxDistance, repetitions,
+		t;
 
-	motionBlur.prototype.filter = function(src, dst) {
-		var width, height, inPixels, outPixels, 
-			sinAngle, cosAngle, total, cx, cy, 
-			index, imageRadius, translateX, 
-			translateY, maxDistance, repetitions,
-			t, p;
+	width = src.width;
+	height = src.height;
 
-		width = src.width;
-		height = src.height;
+	inPixels = src;
 
-		inPixels = src.getImageData(0, 0, width, height);
-		console.log(inPixels);
+	sinAngle = Math.sin(this.angle);
+	cosAngle = Math.cos(this.angle);
 
-		sinAngle = Math.sin(this.angle);
-		cosAngle = Math.cos(this.angle);
+	cx = width/2;
+	cy = height/2;
+	index = 0;
 
-		cx = width/2;
-		cy = height/2;
-		index = 0;
+	imageRadius = Math.sqrt( cx*cx + cy*cy );
+	translateX = this.distance * Math.cos( this.angle );
+	translateY = this.distance * -Math.sin( this.angle );
+	maxDistance = this.distance + Math.abs(this.rotation*imageRadius) + imageRadius;	
+	repetitions = maxDistance;
 
-		imageRadius = Math.sqrt( cx*cx + cy*cy );
-		translateX = this.distance * Math.cos( this.angle );
-		translateY = this.distance * -Math.sin( this.angle );
-		maxDistance = this.distance + Math.abs(this.rotation*imageRadius) + imageRadius;	
-		repetitions = maxDistance;
+	repetitions = 10;
 
-		for (var y = 0; y < height; y++) {
-			for (var x = 0; x < width; x++) {
-				var a = 0; var r = 0; var g = 0; var b = 0;
-				var count = 0;
+	var p = "";
+	for (var y = 0; y < height; y++) {
+		for (var x = 0; x < width; x++) {
+			var a = 0; var r = 0; var g = 0; var b = 0;
+			var count = 0;
 
-				for (var i = 0; i < repetitions; i++) {
-					var newX = x; var newY = y;
-					var f = i/repetitions;
+			for (var i = 0; i < repetitions; i++) {
+				var newX = x; var newY = y;
+				var f = i/repetitions;
 
-					p.x = x;
-					p.y = y;
+				p.x = x;
+				p.y = y;
 
+				//console.log("lol");
 
-
-				};
 
 			};
-		};
 
+		};
 	};
 
-	// var test = new motionBlur();
-	// test.filter(1, 1, 1, true, false);
-
-});
+};
